8cb1e41eaeca5329fd0424863d8107cd99be2e28
agrackle <64942985+agrackle@users.noreply.github.com>
2021-09-26 15:22:42 -0400

    spyder file

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Alexander Keller (alexander.keller@duke.edu)
+# Date:   Fall 2021
+#--------------------------------------------------------------
